24c53e072f914f292424c61acd54f67b0b9042a4
Oliver Polka <olip@ciklum.com>
2023-09-14 14:51:25 +0200

    Fix

--- src/Gir/Encoders.fs
+++ src/Gir/Encoders.fs
@@ -16,7 +16,8 @@ let productEncoder (product: Product) =
         [ "productId", Encode.int product.ProductId
           "name", Encode.string product.Name
           "price", Encode.decimal product.Price
-          "img", Encode.string product.Img ]
+          "img", Encode.string product.Img
+          "bigImg", Encode.string product.BigImg ]
 
 let cartItemEncoder (cartItem: CartItem) =
     Encode.object
